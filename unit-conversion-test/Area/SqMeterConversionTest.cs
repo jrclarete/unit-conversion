a018313f3eff1fb851303254dcba3a131a9420d6
jrclarete <jhonrommel.clarete@gmail.com>
2023-02-07 06:36:20 +0800

    Created test for SqMeter to SqMillimeter.

---
+++ unit-conversion-test/Area/SqMeterConversionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using unit_conversion.Enums;
+using unit_conversion;
+
+namespace unit_conversion_test.Area
+{
+    public class SqMeterConversionTest
+    {
+        [Fact]
+        public void ConvertToSqMillimeter()
+        {
+            // Act
+            decimal toSqMillimeter = AreaConversion.convert(AreaUnit.SQMETER, AreaUnit.SQMILLIMETER, 5);
+
+            // Assert
+            Assert.Equal(5000000m, toSqMillimeter);
+        }
+
+        [Fact]
+        public void ConvertToSqMeter()
+        {
+            // Act
+            decimal toSqMeter = AreaConversion.convert(AreaUnit.SQMETER, AreaUnit.SQMETER, 5);
+
+            // Assert
+            Assert.Equal(5m, toSqMeter);
+        }
+    }
+}
